1a6660dd2235ae93642b2bd74535a12aa0e66228
Mohammad Hosein Lashani <sapumr@gmail.com>
2025-01-11 18:29:14 +0330

    Add DATABASE_URL to server environment variables and update postinstall script for Prisma

--- src/utils/env/serverEnv.ts
+++ src/utils/env/serverEnv.ts
@@ -6,6 +6,7 @@ export const serverEnv = createEnv({
     DISCORD_BOT_TOKEN: z.string().min(1),
     DISCORD_ALLOWED_CHANNELS: z.string().min(1),
     DISCORD_BOT_ERROR_LOGS_CHANNEL_ID: z.string().min(1),
+    DATABASE_URL: z.string().min(1),
   },
   experimental__runtimeEnv: process.env,
 });
